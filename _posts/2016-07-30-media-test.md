cc242599fa06144fa6dcadfa890633799c750ef5
Thames1990 <ThomasMohr1990@gmail.com>
2016-08-02 20:26:29 +0200

    Improve team media overview Changed JQuery call to external file Changed paginator text to fix mobile view errors

--- _posts/2016-07-30-media-test.md
+++ _posts/2016-07-30-media-test.md
@@ -1,36 +1,8 @@
 ---
 layout: post
 title: Multimedia Test
-tags: [Mutlimedia, Test, football-data]
+tags: [Multimedia, Test, football-data]
 ---
 
 <script src="/js/jquery-1.11.2.min.js"></script>
-
-<script>
-    var competition_ids = ["424", "426", "427", "428", "430", "431", "432", "433", "434", "435", "436", "437", "438"]
-    $.each(competition_ids, function(index, item) {
-        $.ajax({
-            headers: { 'X-Auth-Token': 'bf0513ea0ba6457fb4ae6d380cca8365' },
-            url: '//api.football-data.org/v1/competitions/' + item + '/teams',
-            dataType: 'json',
-            type: 'GET',
-        }).done(function(response) {
-            console.log(response);
-            $.ajax({
-                headers: { 'X-Auth-Token': 'bf0513ea0ba6457fb4ae6d380cca8365' },
-                url: '//api.football-data.org/v1/competitions/' + item,
-                dataType: 'json',
-                type: 'GET',
-            }).done(function(response) {
-                $('.image_grid').append('<h2>' + response.caption + '</h2>');
-            });
-            $.each(response.teams, function(index, item) {
-                $('.image_grid').append(
-                '<a href="' + response.teams[index].crestUrl + '"><figure><img src="' + response.teams[index].crestUrl + '"/><figcaption>' + response.teams[index].name + '</figcaption></figure></a>'
-                );
-            });
-        });
-    });
-</script>
-
-<div class="image_grid"></div>
+<script src="/js/media-test.js"></script>
---
+++ js/media-test.js
@@ -0,0 +1,38 @@
+var competition_ids = [424, 426, 427, 428, 430, 431, 432, 433, 434, 435, 436, 437, 438];
+// var competition_ids = [424];
+$('body > header > div > div.container').append('<div class="image_grid"></div>');
+$.each(competition_ids, function (competition_index, competition) {
+    $('.image_grid').append('<div class="' + competition + '"></div>');
+    $.ajax({
+        headers: {'X-Auth-Token': 'bf0513ea0ba6457fb4ae6d380cca8365'},
+        url: '//api.football-data.org/v1/competitions/' + competition + '/teams',
+        dataType: 'json',
+        type: 'GET'
+    }).done(function (response) {
+        console.log(response);
+        $.ajax({
+            headers: {'X-Auth-Token': 'bf0513ea0ba6457fb4ae6d380cca8365'},
+            url: '//api.football-data.org/v1/competitions/' + competition,
+            dataType: 'json',
+            type: 'GET'
+        }).done(function (response) {
+            $('.' + competition).before(
+                '<h2 class="' + competition + '" onclick="toggle_div(' + competition + ')" style="cursor: crosshair">' + response.caption + '</h2>'
+            ).hide();
+        });
+        $.each(response.teams, function (team_index, team_item) {
+            $('.' + competition).append(
+                '<a href="' + response.teams[team_index].crestUrl + '">' +
+                '<figure>' +
+                '<img src="' + response.teams[team_index].crestUrl + '"/>' +
+                '<figcaption>' + response.teams[team_index].name + '</figcaption>' +
+                '</figure>' +
+                '</a>'
+            );
+        });
+    });
+});
+
+var toggle_div = function (competition_item) {
+    $('div.' + competition_item).toggle();
+};
