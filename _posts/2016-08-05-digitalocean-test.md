e99c514167681b988a31d9be78bf18b4060d06de
Thames1990 <ThomasMohr1990@gmail.com>
2016-08-05 13:45:33 +0200

    Add Digitalocean test

---
+++ _posts/2016-08-05-digitalocean-test.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title: Digitalocean Test
+date: 2016-08-05 13:28
+js:
+    - /js/jquery-3.1.0.min.js
+    - /js/konflux.min.js
+    - /js/digitalocean.js
+    - /js/digitalocean-test.js
+---
---
+++ js/digitalocean-test.js
@@ -0,0 +1,27 @@
+//  see of we can obtain the token from localStorage
+var token = localStorage.getItem('TOKEN');
+
+//  if no token is found, we ask for it
+if (!token) {
+    token = prompt('Your DigitalOcean API token please');
+
+    //  ask permission to store the token in localStorage
+    if (confirm('Do you want to save this token in localStorage?'))
+        localStorage.setItem('TOKEN', token);
+}
+
+//  and finally pass on the token to the DOv2 instance.
+DOv2.token(token);
+
+DOv2.Droplets.list(function (error, result, next) {
+    if (!error) {
+        console.log(result);  //  Contains an array of Droplet items
+        $('body > div > div > div > article').append('<pre>' + JSON.stringify(result, null, "\t") + '</pre>');
+    }
+
+    if (next) {
+        //  next is a function which expects a callback function and will retrieve the next items for this call
+        //  in this case: Droplets
+        console.log('and there are more..');
+    }
+});
